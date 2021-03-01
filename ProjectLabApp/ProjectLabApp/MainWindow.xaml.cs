1d2c770b7bdb6820f2f51bf08d47c74d228477ea
Yifang Meng <yifangmeng@gmail.com>
2021-03-01 13:04:07 +0100

    general scheme

--- ProjectLabApp/ProjectLabApp/MainWindow.xaml.cs
+++ ProjectLabApp/ProjectLabApp/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProjectLabApp
 {
@@ -20,9 +25,52 @@ namespace ProjectLabApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        int gameSpeed = 15;
+        int playerSpeed = 10;
+        int numOfCar;
+        int star;
+        int reward;
+        double score;
+
+        bool moveLeft;
+        bool moveRight;
+        bool gameOver;
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public void GameStart()
+        {
+            moveLeft = false;
+            moveRight = false;
+            gameOver = false;
+            score = 0;
+        }
+
+        private void gameBoard_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = true;
+            }
+        }
+
+        private void gameBoard_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = false;
+            }
         }
     }
 }
